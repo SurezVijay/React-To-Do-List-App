556499e97bbf9e60e685ee49f15377be35d784e3
SurezVijay <v.suresh2907@gmail.com>
2020-04-26 17:29:50 +0530

    Updates

---
+++ precache-manifest.a13a0ed7489356b8d33a7fc368fb85d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30297e362cc957697633f46f2f848312",
+    "url": "/React-To-Do-List-App/index.html"
+  },
+  {
+    "revision": "ffc13cbe29b9990b1953",
+    "url": "/React-To-Do-List-App/static/css/main.7cfc71cd.chunk.css"
+  },
+  {
+    "revision": "18c94cb9c4d365db6f70",
+    "url": "/React-To-Do-List-App/static/js/2.7a5e5331.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-To-Do-List-App/static/js/2.7a5e5331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc13cbe29b9990b1953",
+    "url": "/React-To-Do-List-App/static/js/main.146fb21c.chunk.js"
+  },
+  {
+    "revision": "fcd0c43dffa2b40c95c7",
+    "url": "/React-To-Do-List-App/static/js/runtime-main.93752d50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-Do-List-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
